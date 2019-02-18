b34ae7baa1dd074b9683dccc2aae75d67816de6b
Airat Shaimuhamitov <shaimuhamitov@gmail.com>
2019-02-19 00:16:48 +0300

    implemented welcome page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FlyInText from './fly-in-text';
+import {CSSTransition} from 'react-transition-group'
 
 class App extends Component {
+
+  state = {
+    appearSection: true
+  }
+
+  toggleAppearSection = () => {
+    this.setState({
+      appearSection: !this.state.appearSection
+    })
+  }
+
   render() {
+    const {appearSection} = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div className="welcome-section">
+        <div className="content-wrap">
+          <FlyInText letters = {['П', 'Р', 'И', 'В', 'Е', 'Т']} ref={instance => {this.child = instance}}/>
+          <CSSTransition
+              in={appearSection}
+              appear={true}
+              timeout ={3000}
+              classNames="fade-descr"
           >
-            Learn React
-          </a>
-        </header>
+            <div className="description">
+              Это тест Айзенка – Русалова. Нажми на кнопку ниже чтобы начать
+            </div>   
+          </CSSTransition>  
+          <CSSTransition
+            in={appearSection}
+            appear={true}
+            timeout={3000}
+            classNames="fade-btn"
+          >
+            <div className="enter-button-section">
+              <a href="#" className="enter-button" onClick={this.handleClick}>Начать тест</a>
+            </div>
+          </CSSTransition>
+        </div>
       </div>
     );
+  }
+
+  handleClick = () => { 
+    this.toggleAppearSection()
+    this.child.handleClick()
+    console.log(this.state.appearSection);
   }
 }
 
---
+++ src/fly-in-text/index.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import {CSSTransition} from 'react-transition-group'
+import './index.css';
+
+class FlyInText extends Component {
+    
+    constructor(props) {
+        super();
+        
+        this.state = {
+            letters: props.letters,
+            appear: true
+        }
+    }
+
+    handleClick() {
+        this.setState({
+            appear: !this.state.appear
+        })
+    }
+
+    render() {        
+        const flyInTextLetters = this.state.letters.map((letter) =>
+            <li key={letter}>{letter}</li>
+        )
+        return(
+            <CSSTransition
+                in={this.state.appear}
+                appear={true}
+                timeout={1000}
+                classNames="fade"
+            >
+                <ul className="fly-in-text">
+                    {flyInTextLetters}
+                </ul>
+            </CSSTransition>
+        );
+    }
+}
+
+export default FlyInText;
