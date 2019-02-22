5caff3ccced0416015035fc62f4e8651cb63b0ea
Airat Shaimuhamitov <shaimuhamitov@gmail.com>
2019-02-22 09:09:08 +0300

    added description section and animation for it

--- src/description-section/description-section-index.js
+++ src/description-section/description-section-index.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react'
 import './description-section-style.css'
+import {CSSTransition} from 'react-transition-group'
 
 export default class DescriptionSection extends Component {
     
   render() {
     return (
-      <div>
-        Hello, it works! :)
+      <div className='description-section'>
+        <CSSTransition
+          in={true}
+          appear={true}
+          timeout={1000}
+          classNames="fade-lable"
+        >
+          <div className="label">
+              <p>Description</p>
+          </div>
+        </CSSTransition>
+        <CSSTransition
+          in={true}
+          appear={true}
+          timeout={1000}
+          classNames="fade-text"
+        >
+          <div className="text">
+            It's description, isn't it?
+          </div>
+        </CSSTransition>
       </div>
     )
   }
